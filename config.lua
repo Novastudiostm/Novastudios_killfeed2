4b6e7e6f32e79062bc7ec1bfcbcb0235ac5f71b8
Novastudios <rotherhamjoshua@gmail.com>
2025-08-20 18:53:44 +0100

    Create config.lua

---
+++ config.lua
@@ -0,0 +1,4 @@
+Config = {}
+
+Config.Framework = 'standalone'  -- Options: 'esx', 'qbcore', 'standalone' (case-sensitive)
+Config.KillfeedDuration = 3500  -- Duration of killfeed display in ms (3000ms fade + 500ms fade-out)
